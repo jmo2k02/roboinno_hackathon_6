eab2145565578977eee4475d2e9ad751b5a7b4c8
Ignacio Dassori <ignacio.dassori.w@gmail.com>
2025-03-14 15:09:54 +0100

    Initial commit

---
+++ DrawSvgOnBoard_robo-innovate.py
@@ -0,0 +1,237 @@
+import os
+import urllib.request
+import math
+import svgpathtools as spt
+from robodk import robolink, robomath, robodialogs
+robolink.import_install('svgpathtools')
+
+# The user of this program is responsible for calibrating the robot to the drawing surface defined by "RW_WhtBrd" in RoboDK
+# Follow these instructions to calibrate the robot to the drawing surface: TODO
+
+
+#-------------------------------------------
+# Settings with User Input
+# IMAGE_FILE = "" # leave empty to be prompted, use this option for testing in the simulator, this option will not work with the real robot
+
+## Specify the SVG file explicitly to run with the real robot. 
+# Bayern Logo old (1923-1954)
+#IMAGE_FILE = "https://upload.wikimedia.org/wikipedia/de/5/56/FC_Bayern_M%C3%BCnchen_Logo_%281923-1954%29.svg"
+# Bayern Logo with starts
+# IMAGE_FILE = "https://upload.wikimedia.org/wikipedia/de/c/c5/FC_Bayern_Muenchen_Wappen_5_Sterne.svg"
+IMAGE_FILE = "svg/flower.svg"
+
+BOARD_WIDTH, BOARD_HEIGHT = 400, 400  # Size of the drawing area
+APPROACH = 25.0  # mm, approach distance for each path, 25mm is a good start
+MM_X_PIXEL = 5.0 # was 5.0 the smaller this value, the more detail the drawing will have but program takes longer.
+
+# simulator settings
+BOARD_BACKGROUND_COLOR = [0, 0, 0, 1]  # Background of the drawing board (R, G, B, A)
+DEFAULT_PATH_COLOR = '#FFFFFF'  # Default drawing colors for path with no styling (should contrast with the background!)
+USE_STYLE_COLOR = True
+PREFER_STROKE_OVER_FILL_COLOR = True  # Prefer using a path stroke color over a path fill color
+TCP_KEEP_TANGENCY = False  # Set to True to keep the tangency along the path
+
+#-------------------------------------------
+# Flig SVG file paths
+def flip_svg_y(paths):
+    flipped_paths = []
+    for path in paths:
+        # Scale X by -1 to flip horizontally
+        flipped_path = path.scaled(-1, 1)  
+        flipped_paths.append(flipped_path)
+    return flipped_paths
+
+# Load the SVG file
+if IMAGE_FILE.startswith('http') and IMAGE_FILE.endswith('.svg'):
+    r = urllib.request.urlretrieve(IMAGE_FILE, "drawing.svg")
+    IMAGE_FILE = "drawing.svg"
+elif not IMAGE_FILE or not os.path.exists(os.path.abspath(IMAGE_FILE)):
+    IMAGE_FILE = robodialogs.getOpenFileName(strtitle='Open SVG File', defaultextension='.svg', filetypes=[('SVG files', '.svg')])
+if not IMAGE_FILE or not os.path.exists(os.path.abspath(IMAGE_FILE)):
+    print("Error: Failed to select SVG File")
+    quit()
+
+paths, path_attribs, svg_attribs = spt.svg2paths2(IMAGE_FILE)
+paths = flip_svg_y(paths)
+
+# Scale the SVG to fit in the desired drawing area
+# 1. Find the bounding area
+xmin, xmax, ymin, ymax = 9e9, 0, 9e9, 0
+for path in paths:
+    _xmin, _xmax, _ymin, _ymax = path.bbox()
+    xmin = min(_xmin, xmin)
+    xmax = max(_xmax, xmax)
+    ymin = min(_ymin, ymin)
+    ymax = max(_ymax, ymax)
+bbox_height, bbox_width = ymax - ymin, xmax - xmin
+
+# 2. Scale the SVG file and recenter it to the drawing board
+SCALE = min(BOARD_HEIGHT / (bbox_height*2) , BOARD_WIDTH / (bbox_width*2))
+svg_height, svg_width = bbox_height * SCALE, bbox_width * SCALE
+svg_height_min, svg_width_min = ymin * SCALE, xmin * SCALE
+#TRANSLATE = complex((BOARD_WIDTH - svg_width) / 2 - svg_width_min, (BOARD_HEIGHT - svg_height) / 2 - svg_height_min)
+TRANSLATE = complex(-svg_width / 2 - svg_width_min, -svg_height / 2 - svg_height_min)
+
+#--------------------------------------------
+# Get RoboDK Items
+RDK = robolink.Robolink()
+RDK.setSelection([])
+
+robot = RDK.ItemUserPick(itemtype_or_list=robolink.ITEM_TYPE_ROBOT)
+tool = robot.getLink(robolink.ITEM_TYPE_TOOL)
+if not robot.Valid() or not tool.Valid():
+    print("No valid robot or tool selected. Exiting.")
+    quit()
+
+# frames = RDK.ItemList(robolink.ITEM_TYPE_FRAME)
+# frames.remove(robot.Parent())
+# frame = RDK.ItemUserPick(itemtype_or_list=frames)  # Reference frame for the drawing
+frame = RDK.Item('RW_WhtBrd')
+if not frame.Valid():
+    print("No valid frame selected. Exiting.")
+    quit()
+
+#-------------------------------------------
+# Setup Drawing Board
+#RDK.Render(False)
+board_draw = RDK.Item('Drawing Board')  # Drawing board
+if board_draw.Valid() and board_draw.Type() == robolink.ITEM_TYPE_OBJECT:
+    board_draw.Delete()
+if not board_draw.Valid():
+    # Check for a predefined whiteboard template in the RoboDK station
+    template_board = RDK.Item('Whiteboard 250mm', itemtype=robolink.ITEM_TYPE_OBJECT)
+    if not template_board.Valid():
+        print("Error: No valid whiteboard template found in the station.")
+        quit()
+    # Copy the template board to the clipboard
+    template_board.Copy()
+    board_draw = frame.Paste()
+    if not board_draw.Valid():
+        print("Error: Failed to paste the Drawing Board.")
+        quit()
+# Configure the board
+board_draw.setVisible(True, False)
+board_draw.setName('Drawing Board')
+board_draw.Scale([BOARD_HEIGHT / 250, BOARD_WIDTH / 250, 1])  # adjust the board size to the image size (scale)
+board_draw.setColor(BOARD_BACKGROUND_COLOR)
+# Apply transformations
+board_pose = board_draw.Pose()
+board_pose = board_pose * robomath.rotx(math.pi)
+board_pose = board_pose * robomath.transl(-BOARD_HEIGHT / 2, -BOARD_WIDTH / 2, 0)  # Shift by -width/2 and -height/2
+board_draw.setPose(board_pose)
+
+#-------------------------------------------
+# Reference pixel object for "drawing"
+pixel_ref = RDK.Item('pixel')  # Reference object to paint
+if not pixel_ref.Valid():
+    print("No pixel reference found. Exiting.")
+    quit()
+#RDK.Render(False)
+
+#-------------------------------------------
+# Initialize the robot
+robot.setPoseFrame(frame)
+robot.setPoseTool(tool)
+# Move to home position
+start_pos = RDK.Item('start_pos')
+robot.MoveJ(start_pos)
+
+#-------------------------------------------
+RDK.ShowMessage(f"Drawing {IMAGE_FILE}..", False)
+
+for path_count, (path, attrib) in enumerate(zip(paths, path_attribs)):
+    styles = {}
+
+    if 'style' not in attrib:
+        if 'fill' in attrib:
+            styles['fill'] = attrib['fill']
+        if 'stroke' in attrib:
+            styles['stroke'] = attrib['stroke']
+    else:
+        for style in attrib['style'].split(';'):
+            style_pair = style.split(':')
+            if len(style_pair) != 2:
+                continue
+            styles[style_pair[0].strip()] = style_pair[1].strip()
+
+    if 'fill' in styles and not styles['fill'].startswith('#'):
+        styles.pop('fill')
+    if 'stroke' in styles and not styles['stroke'].startswith('#'):
+        styles.pop('stroke')
+
+    hex_color = DEFAULT_PATH_COLOR
+    if USE_STYLE_COLOR:
+        if PREFER_STROKE_OVER_FILL_COLOR:
+            if 'stroke' in styles:
+                hex_color = styles['stroke']
+            elif 'fill' in styles:
+                hex_color = styles['fill']
+        else:
+            if 'fill' in styles:
+                hex_color = styles['fill']
+            elif 'stroke' in styles:
+                hex_color = styles['stroke']
+
+    draw_color = spt.misctools.hex2rgb(hex_color)
+    draw_color = [round(x / 255, 4) for x in draw_color]
+
+    if 'id' in attrib:
+        RDK.ShowMessage(f"Drawing {attrib['id']} with color {hex_color}", False)
+    else:
+        RDK.ShowMessage(f"Drawing path {path_count} with color {hex_color}", False)
+
+    approach_done = False
+    prev_point = None
+    for segment in path.scaled(SCALE).translated(TRANSLATE):
+        segment_len = segment.length()
+        steps = int(segment_len / MM_X_PIXEL)
+        if steps < 1:
+            continue
+
+        for i in range(steps + 1):
+            t = 1.0
+            segment.point(t)
+            if i < steps:
+                # We need this check to prevent numerical accuracy going over 1, as t must be bound to [0,1]
+                i_len = segment_len * i / steps
+                t = segment.ilength(i_len)
+
+            point = segment.point(t)
+            py, px = point.real, point.imag
+
+            pa = 0
+            if prev_point:
+                v = point - prev_point
+                norm_v = robomath.sqrt(v.real * v.real + v.imag * v.imag)
+                v = v / norm_v if norm_v > 1e-6 else complex(1, 0)
+                pa = robomath.atan2(v.real, v.imag)
+
+            if not approach_done and i == 0:
+                # Safe approach to the first target
+                target0 = robomath.transl(px, py, 0) * robomath.rotz(pa)
+                target0_app = target0 * robomath.transl(0, 0, -APPROACH)
+                robot.MoveJ(target0_app)
+                approach_done = True
+                continue
+
+            point_pose = robomath.transl(px, py, 0) * robomath.rotz(pa)
+            robot_pose = robomath.transl(px, py, 0) if not TCP_KEEP_TANGENCY else point_pose
+
+            robot.MoveL(robot_pose)
+
+            # add pixel logic only for the actual drawing (exclude the approach point)
+            if approach_done:
+                tool_pose = robot.Pose()  # Get the tool's current pose in the robot's frame
+                tool_pose_relative_to_board = robomath.invH(board_pose) * tool_pose  # Transform to board coordinates
+                pixel_ref.Recolor(draw_color)
+                board_draw.AddGeometry(pixel_ref, tool_pose_relative_to_board)
+
+            prev_point = point
+
+    # Safe retract from the last target
+    if approach_done:
+        target_app = robot_pose * robomath.transl(0, 0, -APPROACH)
+        robot.MoveL(target_app)
+
+robot.MoveJ(start_pos)
+RDK.ShowMessage(f"Done drawing {IMAGE_FILE}!", False)
--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# roboinno_hackathon_6
+# Challenge N°6: No-Code Cobot
+
+**Welcome to the robo.innovate Hackathon 2025!** In this challenge, your team will be tasked with implementing an intuitive user interface for teleoperating a Universal Robots UR10 Cobot.
+
+**Lets get drawing!** The robot is set up with a marker at its end-effector, and its task will be to make drawings on a white board. We provide you with a script which implements the pipeline from image to robot motion.
+
+In this repository you will find instructions and the necessary files to get your simulated UR10 running in the RoboDK software. We have also included some resources that you might find useful during the development of your solution.
+
+## Installing RoboDK
+
+RoboDK is a powerful robotic simulation and offline programming software that allows users to program industrial robots without needing to write complex code manually. It supports a wide range of robot brands (like KUKA, FANUC, ABB, and UR) and enables tasks such as path planning, robot calibration, collision detection, and integration with CAD/CAM software.
+
+Installing and using RoboDK is necessary for this challenge. It allows teams to simulate and test their solutions quickly. You can download it from this [link](https://robodk.com/download). Note that this is a 30-day trial, which should be plenty for our 4-day hackathon.
+
+RoboDK is available for Windows, Mac, and Ubuntu. Feel free to use your prefered platform, but we recommend to use Windows. In our experience it runs a bit faster, and it doesn't have an issue with saving the script that we will see later on.
+
+## Using RoboDK
+
+If everything worked out during installation, when starting RoboDK you should get an empty world with one reference frame.
+
+![](images/RoboDK_empty.jpg)
+
+We have provided on this repository a **.rdk** file. A .rdk file is a project file used by RoboDK to store robot simulations, programs, tool definitions, and workcell setups. It acts as a container that saves all the components of a RoboDK project, including 3D models, robot paths, and configurations.
+
+You can open the prepared project in two ways: Double click on the .rdk file provided, or in an empty project go to **>File>..Open** and select the corresponding file. You should see the following setup.
+
+![](images/RoboDK_setup.jpg)
+
+Great! You don't need to worry about most of the things shown. The colored arrows are the reference frames of the base, end-effector, starting points, and points used for calibrating the robot. For less visual clutter, toggle the visibility of the later two by going to their respective element on the project tree (left side), selecting it and pressing F7.
+
+## Drawing SVG on Board script
+
+At the end of the project tree you'll find the *DrawSvgOnBoard_robo-innovate.py* script. Right click on it and select **Edit Python Script** to inspect its contents. You will note that the script defines the variable **IMAGE_FILE**, which determines the SVG file to be drawn by the robot. It can be a local file path or a URL to an external file.
+
+The code imports the **svgpathtools** library. Using the RoboDK built in Python interpreter will not work, since the library will not be found. You can try this by double clicking on the script from the project tree. Since you most likely will require more external libraries as well, we recommend you change the Python interpreter following the instructions in the section **Troubleshooting Python setup in RoboDK** found [here](https://robodk.com/doc/en/RoboDK-API-Python-API.html). To install the library in your own environment, simply
+
+```
+pip install svgpathtools
+```
+
+On the same link, you can find information on changing the text editor to one of your liking.
+
+After setting up Python with the needed libraries, test the script by double clicking on it. The robot should start drawing a flower!
+
+![](images/Drawing.jpg)
+
+Note that the yellow line is not the drawing made by a marker, but rather the path followed by the tool center point, as evidenced by the line coming out of the board.
+
+A nice aspect of simulating is that we can speed up the process. On the toolbar, press the **Fast Simulation** icon for, you guessed it, a faster simulation. You can also set the default speed to one of your liking!
+
+![](images/Speedup.jpg)
+
+**Important:** The implementation of how the robot moves should not be modified. We have provided a pipeline that takes an SVG file as input, and performs the motion of the robot so that it draws said SVG. Your task is to implement a user interface which outputs a valid SVG for the robot to draw. How it outputs it is completely up to you! Search for images online? Make the user draw and convert that to an SVG? Tell an AI to give you a drawing? The freedom is yours!
+
+## Scalable Vector Graphics
+
