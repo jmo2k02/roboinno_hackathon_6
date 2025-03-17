8a9eff79a1a2b02f1a7d8d06cc7d3df13bc37ad7
jmo5k02 <justusmrosk@web.de>
2025-03-17 17:12:39 +0100

    started adding robot endpoint

--- backend/cobot/cobot/api/router.py
+++ backend/cobot/cobot/api/router.py
@@ -3,20 +3,15 @@ from fastapi import APIRouter, Depends
 from cobot.auth.deps import validate_token
 
 from cobot.api.v1.svg import router as svg_router
+from cobot.api.v1.robot import router as robo_router
 
-router = APIRouter(
-    dependencies=[Depends(validate_token)]
-)
+router = APIRouter(dependencies=[Depends(validate_token)])
 
-v1_router = APIRouter(
-    prefix="/api/v1"
-)
+v1_router = APIRouter(prefix="/api/v1")
 
 
 v1_router.include_router(router=svg_router, prefix="/svg", tags=["svg"])
 
+v1_router.include_router(router=robo_router, prefix="/robot", tags=["robot"])
 
 router.include_router(v1_router)
-
-
-
