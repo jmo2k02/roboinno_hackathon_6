694de3af762edd76fc92a0ae7230cb32c35c12d5
dominjack <schiwietzdominik0@gmail.com>
2025-03-17 14:40:51 +0100

    svg route

--- src/backend/cobot/cobot/main.py
+++ src/backend/cobot/cobot/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 from loguru import logger
 from fastapi import FastAPI
+from cobot.api.v1.svg import router as svg_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -21,6 +22,7 @@ def create_app():
     app = FastAPI(
         lifespan=lifespan
     )
+    app.include_router(svg_router)
     
     return app
     
