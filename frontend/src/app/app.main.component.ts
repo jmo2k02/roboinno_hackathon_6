4f13262b3ef5867a3ca68f51661c3d6ca16979c8
“max_praus” <max.praus@spaciv.com>
2025-03-17 18:09:47 +0100

    Enabling routes. Add primeNG

--- frontend/src/app/app.main.component.ts
+++ frontend/src/app/app.main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AppMenuComponent } from "./app-menu.component";
 import { AppTopbarComponent } from "./app-topbar.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main',
-  imports: [AppMenuComponent, AppTopbarComponent],
+  imports: [AppMenuComponent, AppTopbarComponent, RouterOutlet],
   templateUrl: './app.main.component.html',
   styleUrl: './app.main.component.scss'
 })
---
+++ frontend/src/app/modules/components/svg-upload/svg-upload.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-svg-upload',
+  imports: [],
+  templateUrl: './svg-upload.component.html',
+  styleUrl: './svg-upload.component.scss'
+})
+export class SvgUploadComponent {
+
+}
