4816386d5f140e69e52654384b161a7fcb491103
JohnFLeo <jonathan.leopold03@gmail.com>
2025-03-18 11:53:12 +0100

    feat: port angular service to react component

--- frontend/src/routes/_root_layout/voice.tsx
+++ frontend/src/routes/_root_layout/voice.tsx
@@ -1,9 +1,37 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+import { Button } from '@/components/ui/button'
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 export const Route = createFileRoute('/_root_layout/voice')({
   component: RouteComponent,
 })
 
+
+const Dictaphone = () => {
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
+  return (
+    <div>
+      <p>Microphone: {listening ? 'on' : 'off'}</p>
+      <Button onClick={()=>SpeechRecognition.startListening({continuous :true})}>Start</Button>
+      <Button onClick={SpeechRecognition.stopListening}>Stop</Button>
+      <Button onClick={resetTranscript}>Reset</Button>
+      <p>{transcript}</p>
+    </div>
+  );
+};
+export default Dictaphone;
+
+
 function RouteComponent() {
-  return <div>Hello "/_root_layout/voice"!</div>
+  return <Dictaphone/>
 }
