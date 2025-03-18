281fbbc22b6ad07859e9e2cb6ea6b74fecb22f28
JohnFLeo <jonathan.leopold03@gmail.com>
2025-03-18 14:13:45 +0100

    feat: add Button that sends the words captchured by the microphone

--- frontend/src/routes/_root_layout/voice.tsx
+++ frontend/src/routes/_root_layout/voice.tsx
@@ -1,12 +1,13 @@
 import { createFileRoute } from '@tanstack/react-router'
-
+import { useMutation } from '@tanstack/react-query';
+import { generateSvgFromPromptApiV1SvgGenerateFromPromptPostMutation } from '@/client/@tanstack/react-query.gen';
 import { Button } from '@/components/ui/button'
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 export const Route = createFileRoute('/_root_layout/voice')({
   component: RouteComponent,
 })
 
-
+/*
 const Dictaphone = () => {
   const {
     transcript,
@@ -25,35 +26,47 @@ const Dictaphone = () => {
       <Button onClick={()=>SpeechRecognition.startListening({continuous :true})}>Start</Button>
       <Button onClick={SpeechRecognition.stopListening}>Stop</Button>
       <Button onClick={resetTranscript}>Reset</Button>
+      <Button onClick={handleSpeechInput}>SendToRobot</Button>
       <p>{transcript}</p>
     </div>
   );
 };
-export default Dictaphone;
+export default Dictaphone;*/
 
 
 function RouteComponent() {
-  return <Dictaphone/>
-}
-function postVoicetoText(text: string): Promise<void>{
-  // Since this request will send JSON data in the body,
-  // we need to set the `Content-Type` header to `application/json`
-  const headers: Headers = new Headers()
-  headers.set('queries', 'application/json')
-  headers.set('token', '712947291537182')
-
-  const body: BodyInit = text
-  const request: RequestInfo = new Request('/generate_from_prompt', {
-    // We need to set the `method` to `POST` and assign the headers
-    method: 'POST',
-    headers: headers,
-    // Convert the user object to JSON and pass it as the body
-    body: body
+  const mutation = useMutation({
+    ...generateSvgFromPromptApiV1SvgGenerateFromPromptPostMutation()
   })
 
-  // Send the request and print the response
-  return fetch(request)
-    .then(res => {
-      console.log("got response:", res)
-    })
+  const handleSpeechInput = (speech: string) =>
+    mutation.mutate({
+      query: {
+        token: "ersatztoken",
+        md_text: speech
+      }
+  })
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
+  return (
+    <div>
+      <p>Microphone: {listening ? 'on' : 'off'}</p>
+      <Button onClick={()=>SpeechRecognition.startListening({continuous :true})}>Start</Button>
+      <Button onClick={SpeechRecognition.stopListening}>Stop</Button>
+      <Button onClick={resetTranscript}>Reset</Button>
+      <Button onClick={()=>handleSpeechInput(transcript)}>SendToRobot</Button>
+      <p>{transcript}</p>
+    </div>
+  );
 }
+
+
